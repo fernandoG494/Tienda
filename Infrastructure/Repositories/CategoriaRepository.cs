7230f1ced5798c734524b31db46865eb244f6930
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-07-30 12:33:29 -0600

    Repositorios Marca y Categoria

---
+++ Infrastructure/Repositories/CategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories;
+
+internal class CategoriaRepository : GenericRepository<Categoria>, ICategoriaRepository
+{
+    public CategoriaRepository(TiendaContext context) : base(context)
+    {
+    }
+}
