610d3dcd2843c3802437a88cf164e4097aea7003
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-07-30 12:03:28 -0600

    Generic repository implemented

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System.Linq.Expressions;
+
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> CreateAsync(T entity);
+    Task<IEnumerable<T>> GetAllAsync();
+    IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+    void Add(T entity);
+    void AddRange(IEnumerable<T> entities);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+    void Update(T entity);
+}
