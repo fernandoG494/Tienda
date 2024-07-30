74a85e8a8bebe8895347beedd7d6c9f036148a38
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-07-30 12:17:07 -0600

    Repositorio Productos

---
+++ Infrastructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+internal class ProductoRepository : GenericRepository<Producto>, IProductoRepository
+{
+    public ProductoRepository(TiendaContext context) : base(context) {}
+
+    public async Task<IEnumerable<Producto>> GetProductosMasCaros(int cantidad) =>
+        await _context.Productos.OrderByDescending(p => p.Precio).Take(cantidad).ToListAsync();
+}
