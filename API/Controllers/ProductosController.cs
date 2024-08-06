08e8ea169edb59b0216700767bb2b65e686c01f9
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-08-06 09:09:26 -0600

    Controlador Producto - Axctualizar producto

--- API/Controllers/ProductosController.cs
+++ API/Controllers/ProductosController.cs
@@ -13,6 +13,7 @@ public class ProductosController : BaseApiController
         _unitOfWork = unitOfWork;
     }
 
+    // GET api/
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -22,6 +23,7 @@ public class ProductosController : BaseApiController
         return Ok(productos);
     }
 
+    // GET api/:id
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -46,5 +48,23 @@ public class ProductosController : BaseApiController
         }
 
         return CreatedAtAction(nameof(Post), new {id=producto.Id}, producto);
+    }
+
+    // PUT api/Productos
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Producto>> Put(int id, [FromBody]Producto producto)
+    {
+        if (producto == null)
+        {
+            return NotFound();
+        }
+
+        _unitOfWork.Productos.Update(producto);
+        _unitOfWork.Save();
+
+        return producto;
     }
 }
